5a550529e6a25ac4693bc206def83bb0b1251a48
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-09-20 19:06:42 -0500

    3d

---
+++ Frontend/ejercicio2/js/script.js
@@ -0,0 +1,15 @@
+function activateblue() {
+    document.getElementById('moto_azul').style.display="block";
+    document.getElementById('moto_gris').style.display="none";
+    document.getElementById('moto_negra').style.display="none";
+}
+function activategray() {
+    document.getElementById('moto_azul').style.display="none";
+    document.getElementById('moto_gris').style.display="block";
+    document.getElementById('moto_negra').style.display="none";
+}
+function activateblack() {
+    document.getElementById('moto_azul').style.display="none";
+    document.getElementById('moto_gris').style.display="none";
+    document.getElementById('moto_negra').style.display="block";
+}
