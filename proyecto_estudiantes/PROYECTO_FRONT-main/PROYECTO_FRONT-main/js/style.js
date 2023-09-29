816aebea91c24eb3956dc61bcdd12b7830f8dc48
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-09-28 19:44:53 -0500

    segunda

---
+++ proyecto_estudiantes/PROYECTO_FRONT-main/PROYECTO_FRONT-main/js/style.js
@@ -0,0 +1,13 @@
+function myfun() {
+    var ClothesPage = window.open ("", "", "width=1920, height=1080");
+    ClothesPage.blur(); 
+}
+
+function register() {
+    var ClothesPage = window.open ('https://accounts.google.com/InteractiveLogin/signinchooser?elo=1&ifkv=AYZoVhd_MzrVWH3a3tswP_WEcOz2eOu5g4OZ3rCIdgXwP7kZ9rWX2U73KmFBUpniFoX5yaXCezt9&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin',"width=1920, height=1080", register)
+ ClothesPage.blur(); 
+}
+
+function wpp () {
+    window.open ('https://web.whatsapp.com/', wpp)
+}
---
+++ proyecto_estudiantes/PaginaWeb-main/PaginaWeb-main/README.md
@@ -0,0 +1,2 @@
+# FullStack
+Actividades
