21ae225a7e1543d5cecc9e11bb42191ae249549a
AndresPineda <soporte1.rita@correo.udistrital.edu.co>
2023-10-09 19:29:04 -0500

    desarrollo proyecto finanzas

---
+++ Proyecto_general/js/script_menu.js
@@ -0,0 +1,16 @@
+// For toggling and finding number of children and other stuff is done here!
+
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", () => {
+  // The navigation.children.length means the following :-
+  // The children inside a parent are basically an array of elements; So, here I'm finding the length of the array aka how many children are inside the nav bar.
+  //   Yup That's all.
+  navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+  //    Casually Toggling Classes to make them animate on click
+  //   Regular stuff ;)
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
+});
